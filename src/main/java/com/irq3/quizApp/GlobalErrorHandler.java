b525fd2bd27ee8981b8c0abfaac3f6b04e9f9f82
irq3 <gnardoor@gmail.com>
2025-11-20 21:54:47 +0100

    removed OAuth2, it will be implemented later
    Signed-off-by: irq3 <gnardoor@gmail.com>

--- src/main/java/com/irq3/quizApp/GlobalErrorHandler.java
+++ src/main/java/com/irq3/quizApp/GlobalErrorHandler.java
@@ -1,50 +1,57 @@
 package com.irq3.quizApp;
 
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestControllerAdvice
 class GlobalErrorHandler {
 
-//    @ExceptionHandler(MethodArgumentNotValidException.class)
-//    public ResponseEntity<Map<String, Object>> handleValidationExceptions(
-//            MethodArgumentNotValidException ex) {
-//
-//        Map<String, String> fieldErrors = new HashMap<>();
-//        ex.getBindingResult().getAllErrors().forEach((error) -> {
-//            String fieldName = ((FieldError) error).getField();
-//            String errorMessage = error.getDefaultMessage();
-//            fieldErrors.put(fieldName, errorMessage);
-//        });
-//
-//        Map<String, Object> response = new HashMap<>();
-//        response.put("status", HttpStatus.BAD_REQUEST.value());
-//        response.put("errors", fieldErrors);
-//
-//        return ResponseEntity.badRequest().body(response);
-//    }
-//
-//    @ExceptionHandler(AuthorizationDeniedException.class)
-//    public ResponseEntity<String> handleAuthDenied(){
-//        return ResponseEntity.badRequest().body("Access denied");
-//    }
-//    @ExceptionHandler(RuntimeException.class)
-//    public ResponseEntity<String> handleError(RuntimeException runtimeException){
-//        return ResponseEntity.badRequest().body(runtimeException.toString());
-//    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
 
-    @RequestMapping(value = "/test-error", produces = MediaType.APPLICATION_JSON_VALUE)
-    public String errorPage(HttpServletRequest request, Model model) {
-        Object status = request.getAttribute("jakarta.servlet.error.status_code");
-        Object message = request.getAttribute("jakarta.servlet.error.message");
+        Map<String, String> fieldErrors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            fieldErrors.put(fieldName, errorMessage);
+        });
 
-        model.addAttribute("status", status);
-        model.addAttribute("message", message);
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", HttpStatus.BAD_REQUEST.value());
+        response.put("errors", fieldErrors);
 
-        return "custom-error";
+        return ResponseEntity.badRequest().body(response);
     }
+
+    @ExceptionHandler(AuthorizationDeniedException.class)
+    public ResponseEntity<String> handleAuthDenied(){
+        return ResponseEntity.badRequest().body("Access denied");
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleError(RuntimeException runtimeException){
+        return ResponseEntity.badRequest().body(runtimeException.toString());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @GetMapping("error")
+    public void handleBadRequest(HttpMessageNotReadableException e,Model model) {
+        model.addAttribute("message", "wrong URL");
+    }
+
 }
